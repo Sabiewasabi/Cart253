28a8c337dd55bfc232c24700a13545806d897fec
Sabiewasabi <sabrinarath26@gmail.com>
2026-09-08 14:15:17 -0700

    Changes colours to blue

--- Topics/Version-control/version-control-workflow/js/script.js
+++ Topics/Version-control/version-control-workflow/js/script.js
@@ -26,7 +26,7 @@ function setup() {
 */
 function draw() {
     // Make the background black (specified as RGB)
-    background(0, 0, 0);
+    background(0, 0, 200);
 
     // Draw a pyramid
     // How many levels for the pyramid
@@ -42,7 +42,7 @@ function draw() {
         // No line around the levels
         noStroke();
         // Set the fill colour to our shade (RGB)
-        fill(shade, shade, 0);
+        fill(shade, shade, 150);
         // Draw rectangles from the centre
         rectMode(CENTER);
         // Draw the rectangle in the centre of the canvas
@@ -58,7 +58,7 @@ function draw() {
     // No line around the shape
     noStroke();
     // Make it red (RGB)
-    fill(255, 0, 0);
+    fill(0,0,200);
     // Draw a 100x100 circle at the mouse position
     ellipse(mouseX, mouseY, 100, 100);
     pop();
